3d9d20ef4939d8a20b4d99b6ad6bd991550463be
Akshaya <akshayabatharaju@gmail.com>
2025-11-21 18:19:25 +0530

    Initial commit

---
+++ src/main/java/SELAB/JAVA1/App.java
@@ -0,0 +1,10 @@
+package SELAB.JAVA1;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!from Nikhitha 24BD5A6609");
+    }
+}
